f684cc153c926ef57f379e0f741550e640bb8aef
Aurore Malval <omalval@gmail.com>
2020-03-13 01:55:38 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "pages#home"
+  resources :reservations, only: [ :new, :create ]
+  # get "/my_meals", to: "meals#view_my_meals", as: "my_meals"
+  # get "/my_meal/:id", to: "meals#view_my_meal", as: "my_meal"
+  # get "/my_orders", to: "orders#view_my_orders", as: "my_orders"
+
+
+  resources :meals
+
+
+    # resources :reviews, only: [ :new, :create ]
+
 end
